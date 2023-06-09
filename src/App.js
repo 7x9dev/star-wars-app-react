a1c66f6b5e57fb6f08ad285f14f30b0cd9e11a30
7x9dev <103652226+7x9dev@users.noreply.github.com>
2023-06-09 17:55:03 +0300

    Created App project and removed unnecessary files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+   return (
+      <div className="App">
+         <h1>Star warS</h1>
+         <h2>list of characters</h2>
+      </div>
+   );
+}
