382217860d21c0e01ea96ea08d83449936b58956
7x9dev <103652226+7x9dev@users.noreply.github.com>
2023-06-10 19:45:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
+import Character from "./Character";
+import Search from "./Search";
 
 export default function App() {
+   
+
    return (
       <div className="App">
          <h1>Star warS</h1>
-         <h2>list of characters</h2>
+         <h2>characters</h2>
+         <Search />
+         <Character />
       </div>
    );
 }
