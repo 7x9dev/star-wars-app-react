6e95df75ce740547be9fff6a56435e806e92db8e
7x9dev <103652226+7x9dev@users.noreply.github.com>
2023-06-12 19:21:17 +0300

    Added styling

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from "react";
 import "./App.css";
 import Character from "./Character";
 import Search from "./Search";
+import Footer from "./Footer";
 
 export default function App() {
-   
-
    return (
       <div className="App">
-         <h1>Star warS</h1>
-         <h2>characters</h2>
+         <div className="header">
+            <h1>Star warS</h1>
+            <h2>characters</h2>
+         </div>
          <Search />
          <Character />
+         <Footer />
       </div>
    );
 }
