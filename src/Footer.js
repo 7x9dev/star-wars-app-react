b93549996a80f3e6280a0d77188fc6c9767b146c
7x9dev <103652226+7x9dev@users.noreply.github.com>
2023-06-12 22:06:32 +0300

    Footer update

--- src/Footer.js
+++ src/Footer.js
@@ -19,7 +19,7 @@ export default function Footer() {
                target="_blank"
                rel="noreferrer"
             >
-               Valerie&nbsp;D.
+               Valeriia&nbsp;D.
             </a>
          </p>
       </footer>
