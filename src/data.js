c362afa15a0fcfe5d92d207fd7931e6e5fb63e2d
7x9dev <103652226+7x9dev@users.noreply.github.com>
2024-01-30 01:02:28 +0200

    App update

---
+++ src/data.js
@@ -0,0 +1,186 @@
+import riseOfSkywalker from "./films/img/the-rise-of-skywalker.webp";
+import solo from "./films/img/solo.webp";
+import lastJedi from "./films/img/the-last-jedi.webp";
+import rogueOne from "./films/img/rogue-one.webp";
+import forceAwakens from "./films/img/the-force-awakens.webp";
+import revegneOfSith from "./films/img/revenge-of-the-sith.webp";
+import attackOfClones from "./films/img/attack-of-the-clones.webp";
+import phantom from "./films/img/the-phantom.webp";
+import returnOfJedi from "./films/img/return-of-the-jedi.webp";
+import empireStrikesBack from "./films/img/the-empire-strikes.webp";
+import newHope from "./films/img/a-new-hope.webp";
+
+import main from "./about/img/main.webp";
+import darth from "./about/img/darth-vader.webp";
+import grogu from "./about/img/grogu.webp";
+import hanSolo from "./about/img/han-solo.webp";
+import kyloRen from "./about/img/kylo-ren.webp";
+import leiaOrgana from "./about/img/leia-organa.webp";
+import lukeSkywalker from "./about/img/luke-skywalker.webp";
+import poe from "./about/img/poe.webp";
+import yoda from "./about/img/yoda.webp";
+import bb8 from "./about/img/bb8.webp";
+
+
+export const data = [
+   {
+      id: 1,
+      image: riseOfSkywalker,
+      film: "Star Wars: The Rise of Skywalker (Episode IX)",
+      year: "2019",
+      link: "https://www.starwars.com/films/star-wars-episode-ix-the-rise-of-skywalker",
+   },
+
+   {
+      id: 2,
+      image: solo,
+      film: "Solo: A Star Wars Story",
+      year: "2018",
+      link: "https://www.starwars.com/films/solo",
+   },
+
+   {
+      id: 3,
+      image: lastJedi,
+      film: "Star Wars: The Last Jedi (Episode VIII)",
+      year: "2017",
+      link: "https://www.starwars.com/films/star-wars-episode-viii-the-last-jedi",
+   },
+
+   {
+      id: 4,
+      image: rogueOne,
+      film: "Rogue One: A Star Wars Story",
+      year: "2016",
+      link: "https://www.starwars.com/films/rogue-one",
+   },
+
+   {
+      id: 5,
+      image: forceAwakens,
+      film: "Star Wars: The Force Awakens (Episode VII)",
+      year: "2015",
+      link: "https://www.starwars.com/films/star-wars-episode-vii-the-force-awakens",
+   },
+
+   {
+      id: 6,
+      image: revegneOfSith,
+      film: "Star Wars: Revenge of the Sith (Episode III)",
+      year: "2005",
+      link: "https://www.starwars.com/films/star-wars-episode-iii-revenge-of-the-sith",
+   },
+
+   {
+      id: 7,
+      image: attackOfClones,
+      film: "Star Wars: Attack of the Clones (Episode II)",
+      year: "2002",
+      link: "https://www.starwars.com/films/star-wars-episode-ii-attack-of-the-clones",
+   },
+
+   {
+      id: 8,
+      image: phantom,
+      film: "Star Wars: The Phantom Menace (Episode I)",
+      year: "1999",
+      link: "https://www.starwars.com/films/star-wars-episode-i-the-phantom-menace",
+   },
+
+   {
+      id: 9,
+      image: returnOfJedi,
+      film: "Star Wars: Return of the Jedi (Episode VI)",
+      year: "1983",
+      link: "https://www.starwars.com/films/star-wars-episode-vi-return-of-the-jedi",
+   },
+
+   {
+      id: 10,
+      image: empireStrikesBack,
+      film: "Star Wars: The Empire Strikes Back (Episode V)",
+      year: "1980",
+      link: "https://www.starwars.com/films/star-wars-episode-v-the-empire-strikes-back",
+   },
+
+   {
+      id: 11,
+      image: newHope,
+      film: "Star Wars: A New Hope (Episode IV)",
+      year: "1977",
+      link: "https://www.starwars.com/films/star-wars-episode-iv-a-new-hope",
+   },
+
+   {
+      id: 12,
+      poster: main,
+   },
+
+   {
+      id: 13,
+      poster: darth,
+   },
+   {
+      id: 14,
+      poster: grogu,
+   },
+   {
+      id: 15,
+      poster: hanSolo,
+   },
+   {
+      id: 16,
+      poster: kyloRen,
+   },
+   {
+      id: 17,
+      poster: leiaOrgana,
+   },
+   {
+      id: 18,
+      poster: lukeSkywalker,
+   },
+   {
+      id: 19,
+      poster: poe,
+   },
+   {
+      id: 20,
+      poster: yoda,
+   },
+
+   {
+      id: 21,
+      poster: bb8,
+   },
+
+   {
+      id: 22,
+      quote: "☝️ The journey, not the destination, shapes your destiny",
+   },
+
+   {
+      id: 23,
+      quote: "💚 In the flow of the Force, find balance and strength",
+   },
+
+   {
+      id: 24,
+      quote: "🧠 Navigate life's path with mindful steps",
+   },
+
+   {
+      id: 25,
+      quote: "🚀 Adventure awaits, where you least expect it",
+   },
+
+   {
+      id: 26,
+      quote: "📖 To grow, we must unlearn what we have learned",
+   },
+
+   {
+      id: 27,
+      quote: "🦄 Trust in the Force, follow your instincts",
+   },
+];
