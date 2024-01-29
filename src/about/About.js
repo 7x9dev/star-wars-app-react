283955665a543b03e7d7f1b64ab465ee54101c2a
7x9dev <103652226+7x9dev@users.noreply.github.com>
2024-01-30 00:58:25 +0200

    Added "About" page

---
+++ src/about/About.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { data } from "../data";
+import "./About.css";
+import prevBtn from "./slider-prev.svg";
+import nextBtn from "./slider-next.svg";
+
+export default function About() {
+   const [filmPoster, setFilmPoster] = useState(11);
+   const { poster } = data[filmPoster];
+   const minPosterIndex = 11;
+   const maxPosterIndex = 20;
+
+   const aboutContents = [
+      "In a distant galaxy filled with mystical forces, epic battles between the forces of good and evil unfold. Star Wars, a legendary saga created by George Lucas, has captured the hearts and imaginations of fans across generations. From the iconic lightsabers to the powerful Jedi, and the menacing Sith Lords, Star Wars has become a cultural phenomenon.",
+      "Embark on a journey through the stars, where brave rebels stand against the tyrannical Galactic Empire, and ancient prophecies shape the destinies of heroes and villains alike. With its rich lore, diverse planets, and unforgettable characters like Luke Skywalker, Princess Leia, and Darth Vader, Star Wars has become more than just a film series; it's a timeless tale of heroism, redemption, and the enduring battle between the light and the dark sides of the Force.",
+      "Explore the vast universe of Star Wars, where the Force binds us all, and every step is a part of an epic adventure that continues to captivate audiences and inspire new generations. May the Force be with you on this journey into the heart of Star Wars lore!",
+   ];
+
+   const nextFilmPoster = () => {
+      setFilmPoster((filmPoster) => {
+         filmPoster++;
+         if (filmPoster > maxPosterIndex) {
+            filmPoster = minPosterIndex;
+         }
+         return filmPoster;
+      });
+   };
+
+   const prevFilmPoster = () => {
+      setFilmPoster((filmPoster) => {
+         filmPoster--;
+         if (filmPoster < minPosterIndex) {
+            return maxPosterIndex;
+         }
+         return filmPoster;
+      });
+   };
+
+   return (
+      <div className="page-wrapper _container _about-container">
+         <h3 className="header">May the Force be with you</h3>
+         <div className="film-slider-container">
+            <button
+               onClick={prevFilmPoster}
+               className="slider-button button-outline"
+            >
+               <img src={prevBtn} alt="prev" />
+            </button>
+            <img src={poster} alt="poster" className="slider-img" />
+            <button
+               onClick={nextFilmPoster}
+               className="slider-button button-outline"
+            >
+               <img src={nextBtn} alt="next" />
+            </button>
+         </div>
+
+         {aboutContents.map((content, index) => (
+            <div key={index} className="about-content">
+               <p>{content}</p>
+            </div>
+         ))}
+
+         <div className="about-btn">
+            <button
+               onClick={() =>
+                  window.open("https://www.starwars.com/", "_blank")
+               }
+               className="button-outline"
+            >
+               Official website
+            </button>
+            <button
+               onClick={() =>
+                  window.open(
+                     "https://starwars.fandom.com/wiki/Main_Page",
+                     "_blank"
+                  )
+               }
+               className="button-outline"
+            >
+               Wookieepedia
+            </button>
+         </div>
+      </div>
+   );
+}
